33ddd79954e4e2ad9ab78aba57f12feb0f45d7e5
kevindarmawan <kevin.darmawan@shopee.com>
2025-01-04 20:57:08 +0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Kevin Darmawan on 04/01/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
