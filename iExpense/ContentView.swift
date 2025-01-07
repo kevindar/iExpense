3e0797b9ea7e8f23a5e3a25b9044032c6dd11bd1
Kevin Darmawan <kevindar2705@gmail.com>
2025-01-07 23:55:21 +0700

    Project 7 part 2 day 37, implement to app

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,16 +6,128 @@
 //
 
 import SwiftUI
-//reset
-struct ContentView: View {
+
+struct AddView: View {
+    
+    var expenses: Expenses
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = ["Personal", "Family", "Dating"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("type in expense details")
+                }
+                Section {
+                    TextField("Name", text: $name)
+                    
+                    Picker("Type", selection: $type) {
+                        ForEach(types, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    
+                    TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let expense = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(expense)
+                    dismiss()
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+    
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack{
+                List {
+                    ForEach(expenses.items) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                if item.type == "Personal" {
+                                    Label("Personal", systemImage: "person.fill")
+                                        .font(.subheadline)
+                                }
+                                if item.type == "Family" {
+                                    Label("Family", systemImage: "figure.2.and.child.holdinghands")
+                                        .font(.subheadline)
+                                }
+                                if item.type == "Dating" {
+                                    Label("Dating", systemImage: "heart.circle")
+                                        .font(.subheadline)
+                                }
+                                Text(item.name)
+                                    .font(.headline)
+                            }
+                            Spacer()
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                }
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                ToolbarItem {
+                    Button("Add expense", systemImage: "plus.square") {
+                        showingAddExpense.toggle()
+                    }
+                    .sheet(isPresented: $showingAddExpense) {
+                        AddView(expenses: expenses)
+                    }
+                }
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
